5f0489a072d969d97dc6353fa88975ea5fa21102
minzzang <minjjang1117@gmail.com>
2022-04-17 17:25:16 +0900

    feat: 타자와 투수 추가
    - 투수가 공을 던지면 multipleBalls를 생성
    - 타자가 공을 치면 multipleBalls를 생성

--- src/main/java/baseball/domain/Batter.java
+++ src/main/java/baseball/domain/Batter.java
@@ -1,4 +1,25 @@
 package baseball.domain;
 
+import baseball.domain.ball.BallGenerator;
+import baseball.domain.ball.MultipleBalls;
+import baseball.domain.picker.ArrayNumberPicker;
+import baseball.domain.picker.NumberPicker;
+
 public class Batter {
+
+    private MultipleBalls multipleBalls;
+
+    public void hitBall(int[] balls) {
+        NumberPicker numberPicker = new ArrayNumberPicker(balls);
+        BallGenerator ballGenerator = new BallGenerator(numberPicker);
+
+        multipleBalls = ballGenerator.generate();
+    }
+
+    public MultipleBalls getMultipleBalls() {
+        if (multipleBalls == null) {
+            throw new IllegalStateException("타자가 아직 준비가 안되었습니다.");
+        }
+        return multipleBalls;
+    }
 }
--- src/main/java/baseball/domain/Pitcher.java
+++ src/main/java/baseball/domain/Pitcher.java
@@ -1,4 +1,25 @@
 package baseball.domain;
 
+import baseball.domain.ball.BallGenerator;
+import baseball.domain.ball.MultipleBalls;
+import baseball.domain.picker.NumberPicker;
+import baseball.domain.picker.RandomNumberPicker;
+
 public class Pitcher {
+
+    private MultipleBalls multipleBalls;
+
+    public void throwBall() {
+        NumberPicker numberPicker = new RandomNumberPicker();
+        BallGenerator ballGenerator = new BallGenerator(numberPicker);
+
+        multipleBalls = ballGenerator.generate();
+    }
+
+    public MultipleBalls getMultipleBalls() {
+        if (multipleBalls == null) {
+            throw new IllegalStateException("투수가 아직 준비가 안되었습니다.");
+        }
+        return multipleBalls;
+    }
 }
