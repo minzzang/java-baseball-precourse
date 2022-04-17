55633b271ff1f7cf8f6939300fabe226a68aa02a
minzzang <minjjang1117@gmail.com>
2022-04-17 18:01:46 +0900

    feat: Referee 추가
    - 타자와 투수의 공을 보고 innings마다 strike, ball 판별
    - 게임이 종료 여부 판단

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -1,4 +1,43 @@
 package baseball.domain;
 
+import baseball.domain.ball.MultipleBalls;
+import baseball.domain.game.Innings;
+
+import static baseball.config.BaseBallConfig.BALL_SIZE;
+
 public class Referee {
+
+    public void referee(MultipleBalls pitcherBalls, MultipleBalls batterBalls, Innings innings) {
+        for (int i = 0; i < BALL_SIZE; i++) {
+            int pitcherNumber = pitcherBalls.findByIndex(i);
+            int batterNumber = batterBalls.findByIndex(i);
+
+            checkStrike(pitcherNumber, batterNumber, innings);
+            checkBall(pitcherBalls, pitcherNumber, batterNumber, innings);
+        }
+    }
+
+    private void checkStrike(int pitcherNumber, int batterNumber, Innings innings) {
+        if (pitcherNumber == batterNumber) {
+            innings.strike();
+        }
+    }
+
+    private void checkBall(MultipleBalls pitcherBalls, int pitcherNumber, int batterNumber, Innings innings) {
+        if (pitcherNumber == batterNumber) {
+            return;
+        }
+        if (pitcherBalls.contains(batterNumber)) {
+            innings.ball();
+        }
+    }
+
+    public boolean isEnded(Innings innings) {
+        if (innings == null) {
+            return false;
+        }
+
+        return innings.getBallCount() == 0
+                && innings.getStrikeCount() == BALL_SIZE;
+    }
 }
