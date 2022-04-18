4ff1d19eb8555e9742ba782f671d1e29e1af7e00
minzzang <minjjang1117@gmail.com>
2022-04-18 21:43:06 +0900

    feat: Application 게임 시작 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseball.view.GameCommand;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameCommand.START.command();
     }
+
 }
