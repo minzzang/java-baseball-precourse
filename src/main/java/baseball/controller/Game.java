80e348604571e3f3838df77bca17fa27efde6291
minzzang <minjjang1117@gmail.com>
2022-04-18 21:17:02 +0900

    feat: Game Controller 추가
    - 여러 도메인과 협력하여 게임을 진행하는 도메인
    - 컨트롤러 역할을 함

---
+++ src/main/java/baseball/controller/Game.java
@@ -0,0 +1,45 @@
+package baseball.controller;
+
+import baseball.domain.Batter;
+import baseball.domain.Innings;
+import baseball.domain.Pitcher;
+import baseball.domain.Referee;
+
+public class Game {
+
+    private Pitcher pitcher;
+    private Referee referee;
+    private Innings innings;
+
+    private Game() {}
+
+    public static Game create() {
+        Game game = new Game();
+        game.pitcher = new Pitcher();
+        game.referee = new Referee();
+        game.innings = new Innings();
+
+        return game;
+    }
+
+    public void run() {
+        pitcher.throwBall();
+    }
+
+    public void playBall(int[] batterBalls) {
+        innings.init();
+
+        Batter batter = new Batter();
+        batter.hitBall(batterBalls);
+
+        referee.referee(pitcher.getMultipleBalls(), batter.getMultipleBalls(), innings);
+    }
+
+    public boolean isEnded() {
+        return referee.isEnded(innings);
+    }
+
+    public Innings getInnings() {
+        return innings;
+    }
+}
